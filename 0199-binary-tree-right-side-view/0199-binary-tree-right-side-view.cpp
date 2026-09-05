9b0d0fd24952e0cfe55fbd853aa63472b7bd771e
RANjeet4518 <ranjeetkumaritwan2@gmail.com>
2026-09-05 15:10:04 +0530

    Time: 0 ms (100%), Space: 14.9 MB (63.71%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -1,33 +1,27 @@
 
 class Solution {
 public:
-void  PrintAllNodesFromRightSide(TreeNode* root,vector<int>& ans,int curr,int level,int& count){
+// int printlevel(TreeNode* root){
+//     if(root==nullptr){
+//         return 0;
+//     }
+//     return 1+max(printlevel(root->left),printlevel(root->right));
+// }
+void  PrintAllNodesFromRightSide(TreeNode* root,vector<int>& ans,int level){
     if(root==nullptr){
         return;
     }
-    if(count==0 && curr==level){
-        ans.push_back(root->val);
-        count++;
+    if(ans.size()==level){
+     ans.push_back(root->val);
     }
-    if(count==0)  PrintAllNodesFromRightSide(root->right,ans,curr+1,level,count);
-    if(count==0)  PrintAllNodesFromRightSide(root->left,ans,curr+1,level,count);
+     PrintAllNodesFromRightSide(root->right,ans,level+1);
+     PrintAllNodesFromRightSide(root->left,ans,level+1);
 
-}
-int NoOfLevel(TreeNode* root){
-    if(root==nullptr){
-        return 0;
-    }
-    return 1+max(NoOfLevel(root->left),NoOfLevel(root->right));
 }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        int n=NoOfLevel(root);
-        int count;
-         for(int i=1;i<=n;i++){
-            count=0;
-             PrintAllNodesFromRightSide(root,ans,1,i,count);
-         }
-        return ans;
+     PrintAllNodesFromRightSide(root,ans,0);
+     return ans;
         
     }
 };
