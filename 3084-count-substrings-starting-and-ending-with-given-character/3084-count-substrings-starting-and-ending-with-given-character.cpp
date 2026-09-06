3d69485731fe15eecec8885968aa8e8812942c1c
RANjeet4518 <ranjeetkumaritwan2@gmail.com>
2026-09-06 14:32:52 +0530

    Time: 1 ms (37.42%), Space: 13.3 MB (22.84%) - LeetHub

--- 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
+++ 3084-count-substrings-starting-and-ending-with-given-character/3084-count-substrings-starting-and-ending-with-given-character.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     long long countSubstrings(string s, char c) {
-        int n=s.size();
-        long long count=0;
-        long long count1=0;
+        long long count=0, count1=0;
         for(auto &p:s){
-            if(p==c){
-                count=count+1+count1;
-                count1++;
-            }
+            if(p==c){ count=count+1+count1;count1++; }
         }
         return count;
-        
     }
 };
